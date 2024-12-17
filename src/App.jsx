f65efbb26f8c26cbaeab07da85875c0ef4b43bca
klelia <clelia.fradella@gmail.com>
2024-12-17 13:14:36 +0100

    Handle form

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import SectionCounter from "./components/SectionCounter.jsx";
 import AccordionList from "./components/AccordionList.jsx";
 import ColorSection from "./components/ColorSection.jsx";
+import MyForm from "./components/MyForm.jsx";
 function App() {
   return (
     <main className="container">
       <SectionCounter />
       <AccordionList />
       <ColorSection />
+      <MyForm />
     </main>
   );
 }
